b0871e31f1d8ef596dcd1e0e503492eae9c1b9a5
tinytacooo <39066347+tinytacooo@users.noreply.github.com>
2019-03-20 16:11:15 -0400

    Edited tests
    make sure to reset this before turning in

--- PP4Test.cpp
+++ PP4Test.cpp
@@ -2,7 +2,7 @@
 // Test File for PP4
 // Requires the Catch2 header file
 // How to compile: g++ -std=c++17 -Wall -I$(CATCH_SINGLE_INCLUDE) (All cpp files)
-// Example if Catch2 and source files are in this directory and at directory level: 
+// Example if Catch2 and source files are in this directory and at directory level:
 //    Example: g++ -std=c++17 -Wall *.cpp
 // To see what tests were successful and failed, run your executable with the -s flag
 //    Example: a.out -s
@@ -15,7 +15,7 @@
 #include "HuffmanTree.hpp"
 
 #include "TestStrings.hpp"
-// Includes: 
+// Includes:
 // const std::string turing       turingCode        turingTree
 // const std::string dijkstra     dijkstraCode      dijkstraTree
 // const std::string wikipedia    wikipediaCode     wikipediaTree
@@ -28,7 +28,7 @@ TEST_CASE("Project 4 Test Cases for Huffman Coding") {
   SECTION("Should compress the turing string") {
     REQUIRE(t.compress(turing) == turingCode);
   }
-    
+
   SECTION("Should serialize the tree for turing") {
     t.compress(turing);
     REQUIRE(t.serializeTree() == turingTree);
@@ -37,12 +37,12 @@ TEST_CASE("Project 4 Test Cases for Huffman Coding") {
   SECTION("Should decompress the turing string") {
     REQUIRE(t.decompress(turingCode, turingTree) == turing);
   }
-
+/*
   // Dijkstra String Tests
   SECTION("Should compress the dijkstra string") {
     REQUIRE(t.compress(dijkstra) == dijkstraCode);
   }
-    
+
   SECTION("Should serialize the tree for dijkstra") {
     t.compress(dijkstra);
     REQUIRE(t.serializeTree() == dijkstraTree);
@@ -56,7 +56,7 @@ TEST_CASE("Project 4 Test Cases for Huffman Coding") {
   SECTION("Should compress the wikipedia string") {
     REQUIRE(t.compress(wikipedia) == wikipediaCode);
   }
-    
+
   SECTION("Should serialize the tree for wikipedia") {
     t.compress(wikipedia);
     REQUIRE(t.serializeTree() == wikipediaTree);
@@ -70,7 +70,7 @@ TEST_CASE("Project 4 Test Cases for Huffman Coding") {
   SECTION("Should compress the constitution string") {
     REQUIRE(t.compress(constitution) == constitutionCode);
   }
-    
+
   SECTION("Should serialize the tree for constitution") {
     t.compress(constitution);
     REQUIRE(t.serializeTree() == constitutionTree);
@@ -79,7 +79,5 @@ TEST_CASE("Project 4 Test Cases for Huffman Coding") {
   SECTION("Should decompress the constitution string") {
     REQUIRE(t.decompress(constitutionCode, constitutionTree) == constitution);
   }
+ */
 }
-
-
-
